c79a5cc407030afd70d75a04fe48c23ae35e7538
Hadi <112569860+anotherhadi@users.noreply.github.com>
2026-03-16 23:45:05 +0100

    Add open graph & twitter banners
    Signed-off-by: Hadi <112569860+anotherhadi@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://default-creds.hadi.diy",
   output: "server",
   vite: {
     plugins: [tailwindcss()],
